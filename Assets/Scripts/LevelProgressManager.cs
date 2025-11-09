a89129e5ad163ca2926dd155f6cc04824b99404f
Ryan Sahyoun <ryan@gameitup.net>
2025-11-08 21:29:22 -0800

    updated levelscript

--- Assets/Scripts/LevelProgressManager.cs
+++ Assets/Scripts/LevelProgressManager.cs
@@ -4,12 +4,11 @@ public class LevelProgressManager : MonoBehaviour
 {
     public static LevelProgressManager Instance { get; private set; }
 
-    private const string ProgressKey = "HighestLevelUnlocked";
-    public int HighestLevelUnlocked { get; private set; } = 0;
+    public int HighestLevelUnlocked { get; private set; } = 0; // Always start at Level 0
 
     void Awake()
     {
-        // Singleton pattern (keeps one instance between scenes)
+        // Singleton pattern
         if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
@@ -17,8 +16,6 @@ public class LevelProgressManager : MonoBehaviour
         }
         Instance = this;
         DontDestroyOnLoad(gameObject);
-
-        LoadProgress();
     }
 
     public void UnlockNextLevel(int justCompletedLevel)
@@ -27,24 +24,11 @@ public class LevelProgressManager : MonoBehaviour
         if (nextLevel > HighestLevelUnlocked)
         {
             HighestLevelUnlocked = nextLevel;
-            SaveProgress();
         }
-    }
-
-    public void SaveProgress()
-    {
-        PlayerPrefs.SetInt(ProgressKey, HighestLevelUnlocked);
-        PlayerPrefs.Save();
-    }
-
-    public void LoadProgress()
-    {
-        HighestLevelUnlocked = PlayerPrefs.GetInt(ProgressKey, 0);
     }
 
     public void ResetProgress()
     {
-        PlayerPrefs.DeleteKey(ProgressKey);
         HighestLevelUnlocked = 0;
     }
 }
